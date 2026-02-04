090cefa54c084c14e4363c7dde0fb4f4ef8a902b
malik <hassanimtiaz158@gmail.com>
2026-02-05 02:14:46 +0500

    2nd commit

--- code.py
+++ code.py
@@ -9,8 +9,8 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 df = pd.DataFrame(data)
 
 
-#new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
-#df.loc[len(df.index)] = new_row_loc
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
 
 
 #new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
