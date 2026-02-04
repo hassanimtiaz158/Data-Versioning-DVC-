24bd9c64efb4b47708362ddc72943bbeff303ccd
malik <hassanimtiaz158@gmail.com>
2026-02-05 02:18:37 +0500

    3rd commit

--- code.py
+++ code.py
@@ -13,8 +13,8 @@ new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
 
-#new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
-#df.loc[len(df.index)] = new_row_loc2
+new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+df.loc[len(df.index)] = new_row_loc2
 
 
 data_dir = 'data'
